3728668f3cb5b275f8eddcc741ec066f454c1a9d
naveen-vj <167769586+naveen-vj@users.noreply.github.com>
2025-09-16 13:54:54 +0530

    next icon update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    turbo: false
+  },
+  devIndicators: false
+}
 
 export default nextConfig;
